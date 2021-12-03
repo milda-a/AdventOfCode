14578b3df9a1c3507605e848afafb1711f469214
Milda Abromaviciute <milda.abromaviciute@gmail.com>
2021-12-03 15:35:19 +0000

    day3 part 2

--- 2021/python/day_3.py
+++ 2021/python/day_3.py
@@ -2,9 +2,10 @@ from read_data import read_in_data
 
 
 def solve():
+    data = read_in_data('../inputs/day_3.txt')
+
     # part one ✔
     indexes = {}
-    data = read_in_data('../inputs/day_3.txt')
     for line in data:
         for index, value in enumerate(line):
             if index not in indexes:
@@ -31,6 +32,49 @@ def solve():
 
     product = int(gamma_string, 2) * int(epsilon_string, 2)
     print(product)
+
+    # part two ✔
+    oxygen_generator_data = data.copy()
+    co2_scrubber_data = data.copy()
+    global_index_tracker = 0
+
+    def ones_and_zeroes(input_list, index):
+        zero_count = 0
+        one_count = 0
+        for line in input_list:
+            if line[index] == "1":
+                one_count += 1
+            else:
+                zero_count += 1
+        return zero_count, one_count
+
+    while global_index_tracker < len(data[0]):
+        o2_zero, o2_one = ones_and_zeroes(oxygen_generator_data, global_index_tracker)
+        co2_zero, co2_one = ones_and_zeroes(co2_scrubber_data, global_index_tracker)
+
+        if o2_zero > o2_one:
+            for line in oxygen_generator_data.copy():
+                if line[global_index_tracker] == "1" and len(oxygen_generator_data) > 1:
+                    oxygen_generator_data.remove(line)
+        else:
+            for line in oxygen_generator_data.copy():
+                if line[global_index_tracker] == "0" and len(oxygen_generator_data) > 1:
+                    oxygen_generator_data.remove(line)
+
+        if co2_one >= co2_zero:
+            for line in co2_scrubber_data.copy():
+                if line[global_index_tracker] == "1" and len(co2_scrubber_data) > 1:
+                    co2_scrubber_data.remove(line)
+        else:
+            for line in co2_scrubber_data.copy():
+                if line[global_index_tracker] == "0" and len(co2_scrubber_data) > 1:
+                    co2_scrubber_data.remove(line)
+
+        # increase global index
+        global_index_tracker += 1
+
+    print(oxygen_generator_data, co2_scrubber_data)
+    print(int(oxygen_generator_data[0], 2) * int(co2_scrubber_data[0], 2))
 
 
 solve()
