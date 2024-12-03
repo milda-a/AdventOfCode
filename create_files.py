c208138569598d9da8802958697af8bdb456038d
Milda Abromaviciute <mabromav@visa.com>
2024-12-03 09:11:53 +0000

    craete file

---
+++ create_files.py
@@ -0,0 +1,24 @@
+import sys
+
+if __name__ == '__main__':
+    args = sys.argv[1:]
+    if len(args) < 2:
+        print("Please provide year and day number")
+        sys.exit(1)
+    year = args[0]
+    day = args[1]
+    with open(f"{year}/day{day}.py", "w") as f:
+        f.write(f"from read_data import read_in_data\n\n")
+        f.write(f"if __name__ == '__main__':\n")
+        f.write(f"    data = read_in_data('./data/day{day}.txt')\n")
+        f.write(f"    part_1_result = 0\n")
+        f.write(f"    part_2_result = 0\n")
+        f.write(f"\n")
+        f.write(f"    # part 1\n")
+        f.write(f"    print('Part 1: ', part_1_result)\n")
+        f.write(f"\n")
+        f.write(f"    # part 2\n")
+        f.write(f"    print('Part 2: ', part_2_result)\n")
+        f.write(f"\n")
+    with open(f"{year}/data/day{day}.txt", "w") as f:
+        pass
