ef3197e9d7c7368f0e6baeca8c8455fa6869699d
Milda Abromaviciute <mabromav@visa.com>
2024-12-09 08:01:54 +0000

    meh

--- 2024/day6.py
+++ 2024/day6.py
@@ -3,13 +3,17 @@ from pprint import pprint
 
 from read_data import read_in_data
 
+
 if __name__ == "__main__":
     data = read_in_data("./data/day6.txt")
     part_1_result = 0
-    part_2_result = 0
+    last_3_blocks = defaultdict()
 
     # part 1
     guard_index = None
+
+    def is_last_blockers_full():
+        return len(last_3_blocks) == 3
 
     # find starting point
     for line_index, each_line in enumerate(data):
@@ -36,6 +40,60 @@ if __name__ == "__main__":
     current_x = guard_index[0]
     current_y = guard_index[1]
     current_position = positions["up"]
+
+    part_2_result = 0
+
+    def potential_obstruction(x, y, curr_pos):
+        result = 0
+        if curr_pos == "up":
+            last_3_blocks.pop("up", None)
+            result = (
+                    1
+                    if x - last_3_blocks["left"][0]
+                    == last_3_blocks["right"][0] - last_3_blocks["down"][0]
+                    and y - last_3_blocks["left"][1]
+                    == last_3_blocks["right"][1] - last_3_blocks["down"][1]
+                    else 0
+                )
+            if result:
+                last_3_blocks.pop("right", None)
+        elif curr_pos == "right":
+            last_3_blocks.pop("right", None)
+            result = (
+                    1
+                    if last_3_blocks["up"][0] - last_3_blocks["left"][0]
+                    == x - last_3_blocks["down"][0]
+                    and last_3_blocks["up"][1] - last_3_blocks["left"][1]
+                    == y - last_3_blocks["down"][1]
+                    else 0
+                )
+            if result:
+                last_3_blocks.pop("down", None)
+        elif curr_pos == "down":
+            last_3_blocks.pop("down", None)
+            result =  (
+                1
+                if last_3_blocks["up"][0] - x
+                == last_3_blocks["right"][0] - last_3_blocks["down"][0]
+                and last_3_blocks["up"][1] - y
+                == last_3_blocks["right"][1] - last_3_blocks["down"][1]
+                else 0
+            )
+            if result:
+                last_3_blocks.pop("left", None)
+        else:
+            last_3_blocks.pop("left", None)
+            result = (
+                1
+                if last_3_blocks["up"][0] - x
+                == last_3_blocks["down"][0] - last_3_blocks["right"][0]
+                and last_3_blocks["up"][1] - y
+                == last_3_blocks["down"][1] - last_3_blocks["right"][1]
+                else 0
+            )
+            if result:
+                last_3_blocks.pop("up", None)
+        return result
 
     def get_position(x, y):
         if (x, y) == positions["up"]:
@@ -77,9 +135,12 @@ if __name__ == "__main__":
             )
             if next_x < 0 or next_y < 0:
                 raise IndexError
-            if data[next_x][next_y] == "#":
-                blockers[(next_x, next_y)].append(current_position)
+            elif data[next_x][next_y] == "#":
+                last_3_blocks[get_position(*current_position)] = (next_x, next_y)
                 change_position()
+            elif is_last_blockers_full():
+                # generate potential obstruction
+                part_2_result += potential_obstruction(next_x, next_y, get_position(*current_position))
             else:
                 marked_map[next_x] = (
                     marked_map[next_x][:next_y] + "X" + marked_map[next_x][next_y + 1 :]
@@ -98,10 +159,10 @@ if __name__ == "__main__":
                     list_to_investigate = [
                         (next_x, i) for i in range(next_y, len(data[next_x]))
                     ]
-                if blocker_positions := [
+                if potential_obstacles := [
                     i for i in list_to_investigate if i in blockers.keys()
                 ]:
-                    if next_position() in [blockers[i][0] for i in blocker_positions]:
+                    if next_position() in [blockers[i][0] for i in potential_obstacles]:
                         part_2_result += 1
                 current_x = next_x
                 current_y = next_y
--- 2024/day8.py
+++ 2024/day8.py
@@ -11,6 +11,7 @@ line_len = None
 def is_valid(tup):
     return 0 <= tup[0] <= data_len - 1 and 0 <= tup[1] <= line_len - 1
 
+
 def generate_antinodes(tup1, tup2):
     first = tup1
     second = tup2
@@ -26,7 +27,8 @@ def generate_antinodes(tup1, tup2):
         antinodes.append(antinode_b)
         antinode_b = (antinode_b[0] - x_diff, antinode_b[1] - y_diff)
     for each in antinodes:
-        sorted_data['#'].append(each)
+        sorted_data["#"].append(each)
+
 
 def mark(arr):
     if len(arr) == 2:
@@ -34,8 +36,8 @@ def mark(arr):
     elif len(arr) < 2:
         return
     else:
-        for i in range(len(arr)-1):
-            for j in range(i+1, len(arr), 1):
+        for i in range(len(arr) - 1):
+            for j in range(i + 1, len(arr), 1):
                 mark([arr[i], arr[j]])
 
 
